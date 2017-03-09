c73a0663b846d33c1bea3291bd1499e23a5a9f04
Cristiano Motta <cristiano.motta@hotmail.it>
2017-03-09 14:17:43 +0100

    commit

--- StudioWeb/App_Start/WebApiConfig.cs
+++ StudioWeb/App_Start/WebApiConfig.cs
@@ -10,7 +10,7 @@ namespace StudioWeb
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            
             // Web API routes
             config.MapHttpAttributeRoutes();
 
