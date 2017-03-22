1eb19aa7d270bfdfbeeff2bbad82e9e67fd2bbac
Cristiano Motta <cristiano.motta@hotmail.it>
2017-03-22 22:37:23 +0100

    ok

---
+++ StudioWeb/Models/Contacts.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudioWeb.Models
+{
+    public class Contacts
+    {
+        public Guid ContactId { get; set; }
+        public Guid LineId { get; set; }
+        public string Name { get; set; }
+        public string  StateProvinceRegion { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+
+        public DateTime? LastCallDate { get; set; }
+        public string Competence { get; set; }
+        public string RelativePotential { get; set; }
+        public string PersonalFidelity { get; set; }        
+    }
+}
