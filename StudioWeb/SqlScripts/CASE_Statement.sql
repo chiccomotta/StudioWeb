16048049aae564524793ba00d7a3facd27f44631
Cristiano Motta <cristiano.motta@hotmail.it>
2017-03-23 16:48:15 +0100

    ok

---
+++ StudioWeb/SqlScripts/CASE_Statement.sql
@@ -0,0 +1,8 @@
+﻿SELECT top 10 name, 
+	CASE SUBSTRING(Name, 1, 1) 
+		WHEN 'A'      
+		THEN 'name inizia per A'
+                       WHEN 'B' THEN 'name inizia B'
+				ELSE 'non lo so...'
+       END AS msg
+FROM PR.Contacts
